ecfb367cf1d39c5f2ef61847c86431ff78c53736
AlbertoParente <albertoparente20@gmail.com>
2020-01-04 23:29:13 -0300

    Creating method saveProduct, deleteProduct and updateProduct

--- src/main/java/com/product/apirest/repository/ProductRepository.java
+++ src/main/java/com/product/apirest/repository/ProductRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.product.apirest.models.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	Product findById(long id);
 
