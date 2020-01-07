0a74087f36c1158c673e79183691b09ef3ef5269
AlbertoParente <albertoparente20@gmail.com>
2020-01-06 23:48:05 -0300

    Configuration Swagger

--- src/main/java/com/product/apirest/config/SwaggerConfig.java
+++ src/main/java/com/product/apirest/config/SwaggerConfig.java
@@ -1,17 +1,19 @@
 package com.product.apirest.config;
 
-import java.util.ArrayList;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.models.Contact;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -33,7 +35,8 @@ public class SwaggerConfig {
 				"API REST in products registration",
 				"1.0",
 				"Terms of Services",
-				new Contact("Alberto Parente", "albertoparente20@gmail.com"),
+				new Contact("Alberto Parente", "albertoparente20@gmail.com",
+						"https://github.com/AlbertoParente"),
 				"Apache License Version 2.0",
 				"https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
 		);
--- src/main/java/com/product/apirest/resources/ProductResource.java
+++ src/main/java/com/product/apirest/resources/ProductResource.java
@@ -2,7 +2,10 @@ package com.product.apirest.resources;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,35 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 import com.product.apirest.models.Product;
 import com.product.apirest.repository.ProductRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin(origins= "*")
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Product")
 public class ProductResource {
 
 	@Autowired
 	ProductRepository productRepository;
 	
+	@ApiOperation(value="Return List Products")
 	@GetMapping("/products")
 	public List<Product> listProducts() {
 		return productRepository.findAll();
 	}
 	
+	@ApiOperation(value="Return Unique Product")
 	@GetMapping("/product/{id}")
 	public Product listProductId(@PathVariable(value="id") long id) {
 		return productRepository.findById(id);
 	}
 	
+	@ApiOperation(value="Save a Product")
 	@PostMapping("/product")
-	public Product saveProduct(@RequestBody Product product) {
+	public Product saveProduct(@RequestBody @Valid Product product) {
 		return productRepository.save(product);
 	}
 	
+	@ApiOperation(value="Delete a Product")
 	@DeleteMapping("/product")
-	public void deleteProduct(@RequestBody Product product) {
+	public void deleteProduct(@RequestBody @Valid Product product) {
 		productRepository.delete(product);
 	}
 	
+	@ApiOperation(value="Update a Product")
 	@PutMapping("/product")
-	public Product updateProduct(@RequestBody Product product) {
+	public Product updateProduct(@RequestBody @Valid Product product) {
 		return productRepository.save(product);
 	}
 }
